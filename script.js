81659cf0e8db77fe124798c93aa35b4efa13038c
RIZQI FATHONAH SIWI NUGRAHA <147226700+RIZQIFATHONAHSIWINUGRAHA@users.noreply.github.com>
2024-04-16 22:04:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// let message = "Selamat Datang";
+// alert(message + " User");
+
+// let pesaninput = prompt("Masukkan Pesan anda ");
+// console.log(typeof pesaninput);
+
+// console.error("Hati hati");
+// console.warn("Hai salam");
+
+function alert(nama) {
+  console.log("hati - hati " + nama);
+}
+window.alert("dan Hallo");
